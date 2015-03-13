09b26cddebd9b437ab2aa8b283396334f7cb5d70
agustiadi <leeagustiadi@gmail.com>
2015-03-13 14:45:47 +0800

    Connected to Parse Backend

---
+++ Bridging Header.m
@@ -0,0 +1,9 @@
+//
+//  Bridging Header.m
+//  karma
+//
+//  Created by Agustiadi on 13/3/15.
+//  Copyright (c) 2015 Agustiadi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
--- karma/AppDelegate.swift
+++ karma/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        Parse.setApplicationId("EMWyf0nZRZ18BDq8pKuoTArkjL876wwFZPvm0KEM", clientKey: "m7gf9uXwiIMAIM2YChJEVkIk1YMCiQfCzz6GvmsV")
+        
         return true
     }
 
