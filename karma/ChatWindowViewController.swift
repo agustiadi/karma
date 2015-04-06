6d5733a35ec7ce00064f69d16a54137353a0e1ba
agustiadi <leeagustiadi@gmail.com>
2015-04-06 10:55:44 +0800

    added Query of Parse database for message

--- karma/ChatWindowViewController.swift
+++ karma/ChatWindowViewController.swift
@@ -10,13 +10,16 @@ import UIKit
 
 class ChatWindowViewController: UIViewController {
     
+    var itemID = "URkd2R5OLu"
+    
+    var currentUser = PFUser.currentUser()
     var currentUserID = PFUser.currentUser().objectId
     var currentUsername = PFUser.currentUser().username
-    var currentUserProfilPic = UIImage()
+    var currentUserProfilPic = UIImage(named: "profilePlaceholder")
     
-    var otherUserID = String()
-    var otherUserProfilePic = UIImage()
-    var otherUsername = "Test Name"
+    var otherUserID = "5h26UD6IVu"
+    var otherUserProfilePic = UIImage(named: "profilePlaceholder")
+    var otherUsername = "Joel Tan"
     
     @IBOutlet var resultScrollView: UIScrollView!
     @IBOutlet var frameMessageView: UIView!
@@ -28,6 +31,12 @@ class ChatWindowViewController: UIViewController {
     var frameMessageOriginalY: CGFloat = 0
     
     let placeholderLabel = UILabel(frame: CGRectMake(5, 8, 200, 20))
+    
+    var messageX: CGFloat = 37.0
+    var messageY: CGFloat = 26.0
+    
+    var senderArray = [String]()
+    var messageArray = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,6 +46,9 @@ class ChatWindowViewController: UIViewController {
         let theWidth = view.frame.width
         let theHeight = view.frame.height
         let navBarY = navigationController?.navigationBar.frame.maxY
+        
+        let unitW = theWidth/37.5  //10 per unit
+        let unitH = theHeight/66.7 //10 per unit
 
         resultScrollView.frame = CGRectMake(0, 0, theWidth, theHeight - (navBarY! + 50))
         resultScrollView.backgroundColor = UIColor.yellowColor()
@@ -55,8 +67,76 @@ class ChatWindowViewController: UIViewController {
         placeholderLabel.backgroundColor = UIColor.clearColor()
         placeholderLabel.textColor = UIColor.lightGrayColor()
         messageTextView.addSubview(placeholderLabel)
+        
+        
+        //Setting of Profile Pic for Current User
+        if currentUser["profilePic"] != nil {
+            
+            currentUser["profilePic"].getDataInBackgroundWithBlock({
+                (imageData: NSData!, error: NSError!) -> Void in
+                
+                if error == nil {
+                    
+                    let image = UIImage(data: imageData)
+                    //self.profilePicView.image = image
+                    
+                } else {
+                    
+                    println(error)
+                    
+                }
+            })
+            
+            
+        }
+        
+        refreshResult()
 
     }
+    
+    func refreshResult() {
+        
+        let theWidth = view.frame.width
+        let theHeight = view.frame.height
+        
+        let unitW = theWidth/37.5  //10 per unit
+        let unitH = theHeight/66.7 //10 per unit
+        
+        messageX = 37.0
+        messageY = 26.0
+        
+        messageArray.removeAll(keepCapacity: false)
+        senderArray.removeAll(keepCapacity: false)
+        
+        
+        let innerP1 = NSPredicate(format: "sender = %@ AND receiver = %@", currentUserID, otherUserID)
+        var innerQ1 = PFQuery(className: "Message", predicate: innerP1)
+        innerQ1.whereKey("itemID", equalTo: itemID)
+        
+        let innerP2 = NSPredicate(format: "sender = %@ AND receiver = %@", otherUserID, currentUserID)
+        var innerQ2 = PFQuery(className: "Message", predicate: innerP2)
+        innerQ2.whereKey("itemID", equalTo: itemID)
+
+        var query = PFQuery.orQueryWithSubqueries([innerQ1, innerQ2])
+        query.addAscendingOrder("createdAt")
+        query.findObjectsInBackgroundWithBlock({
+            (objects: [AnyObject]!, error: NSError!) -> Void in
+            
+            if error == nil {
+                
+                for object in objects {
+                    
+                    self.senderArray.append(object.objectForKey("sender") as String)
+                    self.messageArray.append(object.objectForKey("message") as String)
+                    
+                }
+
+            }
+            
+        })
+    
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
